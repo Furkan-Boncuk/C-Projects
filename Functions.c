e09394c71d1d98d80dc8ef68dd782b177f4b89af
Furkan Boncuk <114020260+Furkan-Boncuk@users.noreply.github.com>
2022-12-26 10:08:14 +0300

    Update Functions.c

--- Functions.c
+++ Functions.c
@@ -4,29 +4,29 @@
 
 int shortEdge,longEdge;
 void rectangle(int shortEdge,int longEdge){
-	int i,j;
-	for(i=1;i<=longEdge;i++){
-		for(j=1;j<=shortEdge;j++){
-			if(j==1 || j==shortEdge){
-				printf("*");
-			}else if(i==1 || i==longEdge){
-				printf("*");
-			}else{
-				printf(" ");
-			}
-		}
-		printf("\n");
-	}
+    int i,j;
+    for(i=1;i<=longEdge;i++){
+         for(j=1;j<=shortEdge;j++){
+	     if(j==1 || j==shortEdge){
+	         printf("*");
+	     }else if(i==1 || i==longEdge){
+	         printf("*");
+	     }else{
+	         printf(" ");
+	     }
+         } 
+        printf("\n");
+    }
 }
 int main() {
-	printf("Short Edge: ");
-	scanf("%d",&shortEdge);
-	printf("Long Edge: ");
-	scanf("%d",&longEdge);
-	if(longEdge>0 && shortEdge>0 && longEdge>shortEdge){
-		printf("\n\n");
-		rectangle(shortEdge,longEdge);
-	}else{
-		printf("\n\nEntered The Invalid Value");
-	}
+    printf("Short Edge: ");
+    scanf("%d",&shortEdge);
+    printf("Long Edge: ");
+    scanf("%d",&longEdge);
+    if(longEdge>0 && shortEdge>0 && longEdge>shortEdge){
+	printf("\n\n");
+	rectangle(shortEdge,longEdge);
+    }else{
+	printf("\n\nEntered The Invalid Value");
+    }
 }
